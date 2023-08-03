0a382b6e64df97f6bd51340e7712a372828a46ca
Stoyan Vuchev <stoyan.v.vuchev@gmail.com>
2023-08-04 00:07:31 +0300

    Implemented Gradle dependencies & plugins - Implemented Gradle plugins. - Implemented Annotation Processing plugins. - Implemented DI plugins. - Implemented Core dependencies. - Implemented Compose dependencies. - Implemented Google Accompanist dependencies. - Implemented Image loading dependencies. - Implemented Local Storage dependencies. - Implemented DI dependencies. - Implemented Serialization dependencies. - Implemented Retrofit dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,69 +2,142 @@
 plugins {
     alias(libs.plugins.com.android.application)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.org.jetbrains.kotlin.kapt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.dagger.hilt.android)
 }
 
 android {
+
     namespace = "com.stoyanvuchev.kodaschool.recipeapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
+
         applicationId = "com.stoyanvuchev.kodaschool.recipeapp"
         minSdk = 23
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+        vectorDrawables.useSupportLibrary = true
+
     }
 
     buildTypes {
-        release {
-            isMinifyEnabled = false
+
+        debug {
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
+
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
+
+    kotlin {
+        jvmToolchain(17)
     }
+
     buildFeatures {
+        buildConfig = true
         compose = true
     }
+
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
 }
 
 dependencies {
 
+    // Core
+
     implementation(libs.core.ktx)
+    implementation(libs.appcompat)
     implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.google.android.material)
     implementation(libs.activity.compose)
+
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    // Jetpack Compose
+
     implementation(platform(libs.compose.bom))
     implementation(libs.ui)
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+
     androidTestImplementation(platform(libs.compose.bom))
     androidTestImplementation(libs.ui.test.junit4)
+
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+
+    // Google Accompanist
+
+    implementation(libs.google.accompanist.systemUiController)
+    implementation(libs.google.accompanist.navigation.animation)
+
+    // Image loading
+
+    implementation(libs.io.coil.compose)
+
+    // Local Storage
+
+    implementation(libs.androidx.datastore.preferences)
+
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+
+    // DI
+
+    implementation(libs.dagger.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+    kapt(libs.dagger.hilt.compiler)
+
+    // Serialization
+
+    implementation(libs.google.gson)
+
+    // Retrofit
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.moshi)
+    implementation(libs.retrofit.converter.gson)
+
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
+
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas-ksp")
 }
